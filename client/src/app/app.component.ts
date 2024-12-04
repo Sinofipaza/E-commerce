bb259eb246bd9f7927c97dbc120a0425bd587446
ntandomagana <ntandomagana@gmail.com>
2024-12-04 16:29:33 +0200

    fix login page, it  looks too small.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { FooterComponent } from './ui/footer/footer.component';
 import { NavComponent } from './ui/nav/nav.component';
 import {  ReactiveFormsModule} from '@angular/forms';
 import { CartComponent } from './ui/cart/cart.component';
+import { LoginComponent } from './ui/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ReactiveFormsModule,CartComponent, FooterComponent, NavComponent, RouterLink],
+  imports: [RouterOutlet, FooterComponent, NavComponent, RouterLink, ReactiveFormsModule, ReactiveFormsModule,CartComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- client/src/app/ui/login/login.component.ts
+++ client/src/app/ui/login/login.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors, ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+function emailValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const email = control.value;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if (email && !emailPattern.test(email)) {
+      return { invalidEmail: true }; 
+    }
+    return null; 
+  };
+}
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+  userDetails = {
+    email: '',
+    password: ''
+  }
+
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    //this initializes the form
+    this.loginForm = this.fb.group({
+      email: new FormControl ('', [Validators.required, emailValidator()]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+    });
+  }
+
+  onSubmit() : void {
+    if (this.loginForm.valid) {
+      console.log('Form submitted successfully', this.loginForm.value);
+    } else {
+      console.log('Form is invalid', this.loginForm.errors);
+    }
+  }
+}
