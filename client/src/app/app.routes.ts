568d6ca3fa81755581c984e27447eb5dedb1edae
Maboku1111 <mabokuseimela6@gmail.com>
2024-12-04 14:10:31 +0200

    routes have been specified

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProductsPageComponent } from './ui/products-page/products-page.component';
+import { ProductDetailsComponent } from './ui/product-details/product-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {"path": "products", component: ProductsPageComponent, pathMatch: "full"},
+    {"path": "products/:id", component: ProductDetailsComponent, pathMatch: "full"}
+];
