172a5ff96a2198afbe4eb0e7646f6438121d8ead
Maboku1111 <mabokuseimela6@gmail.com>
2024-12-04 15:53:49 +0200

    Products list page shows

--- client/src/app/ui/products-page/products-page.component.ts
+++ client/src/app/ui/products-page/products-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ProductsInterface } from '../../types/products.interface';
 import { ProductsService } from '../../services/products.service';
 import { Router } from '@angular/router';
@@ -10,8 +10,15 @@ import { Router } from '@angular/router';
   templateUrl: './products-page.component.html',
   styleUrl: './products-page.component.css',
 })
-export class ProductsPageComponent {
+export class ProductsPageComponent implements OnInit {
   products: ProductsInterface[] = [];
   
   constructor(private router: Router, private productService: ProductsService) {}
+
+  ngOnInit(): void {
+    this.productService.getProducts()
+      .subscribe(response => {
+        this.products = response;
+      })
+  }
 }
