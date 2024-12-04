c5b1a955c7a9562a63b8f90b6752a9ac3ff70595
Maboku1111 <mabokuseimela6@gmail.com>
2024-12-04 14:25:44 +0200

    Product interface has been created along with it's corresponding services

--- client/src/app/services/products.service.ts
+++ client/src/app/services/products.service.ts
@@ -1,8 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ProductsInterface } from '../types/products.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  constructor() {}
+  url = 'http://localhost:3000/'
+
+  constructor(private http: HttpClient) {}
+
+  getProducts(): Observable<ProductsInterface> {
+    return this.http.get<ProductsInterface>(`${this.url}/products`);
+  }
+
+  getProductById(id: number): Observable<ProductsInterface> {
+    return this.http.get<ProductsInterface>(`${this.url}/products/${id}`);
+  }
 }
---
+++ client/src/app/types/products.interface.ts
@@ -0,0 +1,9 @@
+export interface ProductsInterface {
+    id: number,
+    name: string,
+    price: number,
+    short_description: string,
+    long_description: string,
+    thumbnail_url: string,
+    on_hand: number
+}
