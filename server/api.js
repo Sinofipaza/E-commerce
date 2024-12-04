d1c31c1c3a3506e4391434562af86827e52eb966
Maboku1111 <mabokuseimela6@gmail.com>
2024-12-04 15:36:21 +0200

    Database connection

--- server/api.js
+++ server/api.js
@@ -1,13 +1,39 @@
 import express from "express";
 import cors from "cors";
+import { pool } from "./dbConfig.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-export {app};
+app.get("/products", async (req, res) => {
+  try {
+    const results = await pool.query(
+      "SELECT id, name, price, short_description, thumbnail_url FROM products;"
+    );
+    res.json(results.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+app.get("/products/:id", async (req, res) => {
+  const productId = parseInt(req.params.id, 10);
+  try {
+    const results = await pool.query("SELECT * FROM products WHERE id=$1;", [
+      productId,
+    ]);
+    res.json(results.rows);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "Server error" });
+  }
+});
+
+export { app };
