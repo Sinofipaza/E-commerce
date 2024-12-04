b5a82ba98675ac295eada92804e104791890bb3d
ntandomagana <ntandomagana@gmail.com>
2024-12-04 16:29:33 +0200

    fix login page, it  looks too small.

--- client/src/app/ui/login/login.component.ts
+++ client/src/app/ui/login/login.component.ts
@@ -1,22 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors, ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
-
-
 import { LoginService } from '../../services/login.service';
 
-// function emailValidator(): ValidatorFn {
-//   return (control: AbstractControl): ValidationErrors | null => {
-//     const email = control.value;
-//     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+function emailValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const email = control.value;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-//     if (email && !emailPattern.test(email)) {
-//       return { invalidEmail: true }; 
-//     }
-//     return null; 
-//   };
-// }
+    if (email && !emailPattern.test(email)) {
+      return { invalidEmail: true }; 
+    }
+    return null; 
+  };
+}
 
 @Component({
   selector: 'app-login',
