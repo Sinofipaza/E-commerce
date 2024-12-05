be4e12604caa366c152ffb934d7e417530845ef0
Ntando Magana <ntandomagana@gmail.com>
2024-12-05 17:53:55 +0200

    fix routing, pages dn't router to either

--- client/src/app/ui/login/login.component.ts
+++ client/src/app/ui/login/login.component.ts
@@ -2,17 +2,17 @@ import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
-function emailValidator(): ValidatorFn {
-  return (control: AbstractControl): ValidationErrors | null => {
-    const email = control.value;
-    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+// function emailValidator(): ValidatorFn {
+//   return (control: AbstractControl): ValidationErrors | null => {
+//     const email = control.value;
+//     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-    if (email && !emailPattern.test(email)) {
-      return { invalidEmail: true }; 
-    }
-    return null; 
-  };
-}
+//     if (email && !emailPattern.test(email)) {
+//       return { invalidEmail: true }; 
+//     }
+//     return null; 
+//   };
+// }
 
 @Component({
   selector: 'app-login',
@@ -22,18 +22,13 @@ function emailValidator(): ValidatorFn {
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  userDetails = {
-    email: '',
-    password: ''
-  }
 
   loginForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    //this initializes the form
+   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
-      email: new FormControl ('', [Validators.required, emailValidator()]),
-      password: new FormControl('', [Validators.required, Validators.minLength(6)])
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
