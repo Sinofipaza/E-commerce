033d881057419f2083db90d1aedf7a42b2ac49b9
Ntando Magana <ntandomagana@gmail.com>
2024-12-05 17:53:55 +0200

    fix routing, pages dn't router to either

--- client/src/app/ui/register/register.component.ts
+++ client/src/app/ui/register/register.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  
+
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      surname: ['', [Validators.required]],
+      phone: ['', [Validators.required, Validators.pattern('^[0-9]{10}$')]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6),Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).+$') // At least 1 uppercase, 1 lowercase, 1 digit
+]],
+    });
+  };
+
+  onSubmit(): void {
+    if (this.registerForm.valid) {
+      console.log('Registration data:', this.registerForm.value);
+      alert('Registration successful!');
+    } else {
+      alert('Form is invalid. Please check your inputs.');
+    }
+  }
 }
+
