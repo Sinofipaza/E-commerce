36167719ab99e82ef2d8f9c93cb42e381c2dd099
KhweziOfentse <158312796+KhweziOfentse@users.noreply.github.com>
2024-12-05 13:57:27 +0200

    added routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HelpComponent } from './ui/help/help.component';
+import { AboutComponent } from './ui/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'help', component: HelpComponent },
+  { path: 'about', component: AboutComponent },
+];
