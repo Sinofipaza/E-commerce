4d5158a8023a0f3b5a01e9f33b5f468c57a10ff2
ntandomagana <ntandomagana@gmail.com>
2024-12-06 10:46:10 +0200

    db connected. fix password issue

---
+++ server/dbConfig.js
@@ -0,0 +1,23 @@
+import pkg from "pg";
+import dotenv from "dotenv"; //import dotenv package to load environment variables
+
+dotenv.config();
+
+
+const { Pool } = pkg;
+
+const pool = new Pool ({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+
+})
+
+pool
+    .connect()
+    .then(() => console.log('Connected to the PostgreSQL database.'))
+    .catch(err => console.error('Connection to the PostgreSQL database failed.', err.stack));
+
+export {pool};
--- server/server.js
+++ server/server.js
@@ -1,5 +1,8 @@
 import http from "http";
 import { app } from "./api.js";
 
+const port = 3000;
+
 const server = http.createServer(app).listen(3000);
+console.log('server is running');
 
