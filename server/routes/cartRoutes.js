e1f04b22e84b936c8b5d666611c16b8956268fab
Khetha100 <khethukuthula990@gmail.com>
2024-12-07 04:24:20 +0200

    conflicts fix

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -12,7 +12,6 @@ app.get("/cartItems", async (req, res) => {
       `SELECT * FROM ${tableName} WHERE softDelete=false AND ordered=false AND quantity>0  ORDER BY price ASC`
       // `SELECT * FROM ${tableName} WHERE softDelete=false AND ordered=false AND quantity>0 AND username=\'${email}\' ORDER BY price ASC`
     );
-    console.log(result)
     res.json(result.rows);
   } catch (err) {
     console.error(err.message);
