cdd99c32362b781e5abe11ac693f1b74382a0fb1
Khetha100 <khethukuthula990@gmail.com>
2024-12-07 04:12:57 +0200

    conflict fix

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { OrdersComponent } from './ui/orders/orders.component';
 import { ProductDetailsComponent } from './ui/product-details/product-details.component';
 import { HelpComponent } from './ui/help/help.component';
 import { AboutComponent } from './ui/about/about.component';
+import { ProductsPageComponent } from './ui/products-page/products-page.component';
 
 
 import { LoginComponent } from './ui/login/login.component';
@@ -11,27 +12,29 @@ import { RegisterComponent } from './ui/register/register.component';
 
 
 export const routes: Routes = [
-    {path:"orders",component:OrdersComponent},
-    {
-        path: "cart", component: CartComponent,
-        children: [
-            {path:"orders",component:OrdersComponent}
-        ]
-    },
+  { path: 'orders', component: OrdersComponent },
+  {
+    path: 'cart',
+    component: CartComponent,
+    children: [{ path: 'orders', component: OrdersComponent }],
+  },
 
-    {
-        path: "productsDetails", component: ProductDetailsComponent,
-        children: [
-            {path:"cart",component:CartComponent}
-        ]
-    },
-    { path: 'help', component: HelpComponent },
+  {
+    path: 'productsDetails',
+    component: ProductDetailsComponent,
+    children: [{ path: 'cart', component: CartComponent }],
+  },
+  { path: 'help', component: HelpComponent },
   { path: 'about', component: AboutComponent },
   { path: 'about', component: AboutComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent  },
+  { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'products', component: ProductsPageComponent, pathMatch: 'full' },
+  {
+    path: 'products/:id',
+    component: ProductDetailsComponent,
+    pathMatch: 'full',
+  },
 ];
-
-
 
