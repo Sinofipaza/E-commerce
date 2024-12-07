0f044c3b54ca8c528e1ef2766fa26ec25a81c474
Khetha100 <khethukuthula990@gmail.com>
2024-12-07 16:25:27 +0200

    merged with develop

--- client/src/app/ui/landing-page/landing-page.component.ts
+++ client/src/app/ui/landing-page/landing-page.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { ProductsPageComponent } from '../products-page/products-page.component';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [],
-  templateUrl: './landing-page.component.html',
+  templateUrl: './landing-page.component.html'
+  ,
   styleUrl: './landing-page.component.css',
 })
 export class LandingPageComponent {}
