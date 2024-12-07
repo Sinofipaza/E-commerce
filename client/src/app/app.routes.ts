39130c369aef09b0d6900a45292617a63647241d
sino <macb@MacBook-Pro.local>
2024-12-07 09:05:58 +0200

    created landingPage

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './ui/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent }
+];
