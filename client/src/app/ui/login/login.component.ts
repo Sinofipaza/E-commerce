992fcc40938dd2882572d1024dc59d8000dfb634
Khetha100 <khethukuthula990@gmail.com>
2024-12-07 12:45:55 +0200

    develop merged with cart

--- client/src/app/ui/login/login.component.ts
+++ client/src/app/ui/login/login.component.ts
@@ -1,26 +1,10 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors, ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
-import { LoginService } from '../../services/login.service';
-
-// function emailValidator(): ValidatorFn {
-//   return (control: AbstractControl): ValidationErrors | null => {
-//     const email = control.value;
-//     const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-
-//     if (email && !emailPattern.test(email)) {
-//       return { invalidEmail: true }; 
-//     }
-//     return null; 
-//   };
-// }
-import { FormGroup, FormBuilder, FormControl, Validators, ValidatorFn, AbstractControl, ValidationErrors, ReactiveFormsModule, FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
 import { Router, RouterLink } from '@angular/router';
-
-
 import { LoginService } from '../../services/login.service';
+
+
 
 // function emailValidator(): ValidatorFn {
 //   return (control: AbstractControl): ValidationErrors | null => {
@@ -41,26 +25,6 @@ import { LoginService } from '../../services/login.service';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-
-  loginForm: FormGroup;
-
-   constructor(private fb: FormBuilder, public loginService: LoginService) {
-    this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
-    });
-  }
-
-  onSubmit() : void {
-    if (this.loginForm.valid) {
-      this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe((response) => {})
-    alert('Form submitted successfully');
-    } else {
-      alert('Form is invalid');
-    }
-  }
-}
 export class LoginComponent {
 
   loginForm: FormGroup;
