d41a59004cd2ffdf06291342dc197fee8170ab8e
Ntando Magana <ntandomagana@gmail.com>
2024-12-08 10:06:37 +0200

    started with jwt

--- server/api.js
+++ server/api.js
@@ -3,6 +3,8 @@ import cors from "cors";
 import bodyParser from "body-parser";
 import { pool } from "./database/databaseConnection.js";
 import {app} from "./routes/expressApp.js"
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 // import { pool } from './dbConfig.js'
 
@@ -31,6 +33,18 @@ app.post('/login', async (req, res) => {
         } else {
             res .status(200).json({message: "Login succssful", user: results.rows[0]})
         }
+
+        //this comparess the hashed password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return res.status(401).json({ message: 'Invalid credentials' });
+        }
+
+        const token = jwt.sign(
+          { id: user.id, email: user.email },
+          process.env.JWT_SECRET,
+          { expiresIn: '1h'}
+        )
     }
     catch (err) {
         console.error(err.message);
@@ -41,12 +55,24 @@ app.post('/login', async (req, res) => {
 //when user registers on the platform
 app.post('/register', async (req, res) => {
     const { name, surname, phone_number, email, password } = req.body;
-    console.log({name, surname, phone_number, email, password});
+
     try {
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      
+        // saves user's details into the database
         const result = await pool.query('INSERT INTO users (name, surname, phone_number, email, password) VALUES($1, $2, $3, $4, $5) RETURNING *', 
             [name, surname, phone_number, email, password]
         );
-        res.json(result.rows)
+
+        const token = jwt.sign(
+          { id: result.rows[0].id, email:result.rows[0].id },
+          process.env.JWT_SECRET,
+          { expiresIn: '1h'}
+        );
+
+        req.status(201).json({ message: "User registered successfully", token});
+        // res.json(result.rows)
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server error');
