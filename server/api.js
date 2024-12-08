e871a28eb098d558cace9f57fcb9e35dcc6cdb60
Ntando Magana <ntandomagana@gmail.com>
2024-12-08 11:12:54 +0200

    added auth.js file

--- server/api.js
+++ server/api.js
@@ -26,7 +26,10 @@ app.post('/login', async (req, res) => {
 
     try {
       //find user by email
+      //find user by email
         const results = await pool.query(
+            'SELECT * FROM users WHERE email = $1',
+            [email]
             'SELECT * FROM users WHERE email = $1',
             [email]
         );
@@ -49,24 +52,10 @@ app.post('/login', async (req, res) => {
         );
 
         res.status(200).json({ message: "Login successful", token});
-        } else {
-            res .status(200).json({message: "Login succssful", user: results.rows[0]})
-        }
-
-        //this comparess the hashed password
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-          return res.status(401).json({ message: 'Invalid credentials' });
-        }
-
-        const token = jwt.sign(
-          { id: user.id, email: user.email },
-          process.env.JWT_SECRET,
-          { expiresIn: '1h'}
-        )
     }
     catch (err) {
         console.error(err.message);
+        res.status(500).json('Server error');
         res.status(500).json('Server error');
     }
 });
@@ -85,13 +74,8 @@ app.post('/register', async (req, res) => {
       const hashedPassword = await bcrypt.hash(password, 10);
       
         // saves user's details into the database
-
-    try {
-
-      const hashedPassword = await bcrypt.hash(password, 10);
-      
-        // saves user's details into the database
         const result = await pool.query('INSERT INTO users (name, surname, phone_number, email, password) VALUES($1, $2, $3, $4, $5) RETURNING *', 
+            [name, surname, phone_number, email, hashedPassword]
             [name, surname, phone_number, email, hashedPassword]
         );
 
@@ -102,15 +86,6 @@ app.post('/register', async (req, res) => {
         );
 
         res.status(201).json({ message: "User registered successfully", token});
-        // res.json(result.rows)
-
-        const token = jwt.sign(
-          { id: result.rows[0].id, email:result.rows[0].id },
-          process.env.JWT_SECRET,
-          { expiresIn: '1h'}
-        );
-
-        req.status(201).json({ message: "User registered successfully", token});
         // res.json(result.rows)
     } catch (err) {
         console.error(err.message);
