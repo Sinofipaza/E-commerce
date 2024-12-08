c1a938625e1c476bb3b1776bf79ebab8260b3c58
Ntando Magana <ntandomagana@gmail.com>
2024-12-08 10:26:12 +0200

    fix db issue

--- server/api.js
+++ server/api.js
@@ -8,6 +8,7 @@ import { pool } from "./database/databaseConnection.js";
 import {app} from "./routes/expressApp.js"
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import { verifyToken } from 'auth.js';
 
 import { pool } from './dbConfig.js'
 
@@ -118,7 +119,7 @@ app.post('/register', async (req, res) => {
     
 });
 //email update
-app.patch('/update-email/:id', async (req, res) => {
+app.patch('/update-email/:id', verifyToken, async (req, res) => {
     const { id } = req.params;
     const { email } = req.body; 
     
@@ -139,7 +140,7 @@ app.patch('/update-email/:id', async (req, res) => {
   });
 
   //update user name
-app.patch('/update-name/:id', async (req, res) => {
+app.patch('/update-name/:id', verifyToken, async (req, res) => {
     const { id } = req.params;
     const { name, surname } = req.body; // New name and surname to update
     
@@ -161,7 +162,7 @@ app.patch('/update-name/:id', async (req, res) => {
   
   
   //for deleting user 
-  app.delete("/delete-user/:id", async (req, res) => {
+  app.delete("/delete-user/:id", verifyToken, async (req, res) => {
     const { id } = req.params;
     const result = await pool.query(
       `DELETE FROM users WHERE id = $1 RETURNING *`,
